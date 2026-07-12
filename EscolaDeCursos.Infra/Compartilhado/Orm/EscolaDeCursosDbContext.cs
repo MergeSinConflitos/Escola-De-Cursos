9951ce33750acfa6a0cf52553977497d9ab80d73
Igor Mota <igormello399@gmail.com>
2026-07-12 19:51:04 -0300

    Adicionada configuração do banco e migration do modulo de categoria

--- EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
+++ EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
@@ -1,4 +1,8 @@
 using System.Reflection;
+using EscolaDeCursos.Dominio.Modulos.ModuloCategoria;
+using EscolaDeCursos.Dominio.Modulos.ModuloCurso;
+using EscolaDeCursos.Dominio.Modulos.ModuloEtapa;
+using EscolaDeCursos.Dominio.Modulos.ModuloNivelDeDificuldade;
 using Microsoft.EntityFrameworkCore;
 
 namespace EscolaDeCursos.Infra.Compartilhado.Orm;
@@ -6,8 +10,18 @@ namespace EscolaDeCursos.Infra.Compartilhado.Orm;
 public sealed class EscolaDeCursosDbContext(
     DbContextOptions<EscolaDeCursosDbContext> options) : DbContext(options)
 {
+
+    public DbSet<Categoria> Categorias => Set<Categoria>();
+    public DbSet<NivelDeDificuldade> NivelDeDificuldades => Set<NivelDeDificuldade>();
+    public DbSet<Curso> Cursos => Set<Curso>();
+    public DbSet<Etapa> Etapas => Set<Etapa>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Ignore<NivelDeDificuldade>();
+        modelBuilder.Ignore<Curso>();
+        modelBuilder.Ignore<Etapa>();
+
         Assembly assembly = typeof(EscolaDeCursosDbContext).Assembly;
 
         modelBuilder.ApplyConfigurationsFromAssembly(assembly);
